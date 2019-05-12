b5d1ad9ab07943ea984345c7c7fba88d74954e37
Soria Jose <soriaajose@gmail.com>
2019-05-12 17:42:26 -0300

    "Se crearon test para los atributos de la clase Ciudad"

--- SoriaAeropuerto/src/test/java/test/TestCiudad.java
+++ SoriaAeropuerto/src/test/java/test/TestCiudad.java
@@ -7,11 +7,97 @@ import org.junit.jupiter.api.Test;
 class TestCiudad {
 
 	@Test
-	void test_Parametros_Ciudad() {
+	void test_Atributos_Ciudad() {
 		
+		Ciudad ciudad1 = null;
 		
-		
-		
-	}
+		try {
+			
+			//(IdCIudad, nombre, codigoPostal)
+			ciudad1 = new Ciudad(101, "Chilecito", "5360");
+			fail("Esta linea no deberia correrse");
+			
+		}catch(Exception exception) {
+			
+			fail("Esta linea no deberia correrse");
+	
+		}
 
+		
+		try {
+			
+			//(IdCIudad, nombre, codigoPostal)
+			ciudad1 = new Ciudad(null, "Chilecito", "5360");
+			
+		}catch(ExceptionNulo exception) {
+			
+			assertEquals("El ID de la ciudad no puede ser nulo", ciudad1.getMessage());
+			
+		}
+		
+		
+		try {
+			
+			//(IdCIudad, nombre, codigoPostal)
+			ciudad1 = new Ciudad("", "Chilecito", "5360");
+			
+		}catch(ExceptionVacio exception) {
+			
+			assertEquals("El ID de la ciudad no puede ser vacio", ciudad1.getMessage());
+			
+		}
+		
+		
+		try {
+			
+			//(IdCIudad, nombre, codigoPostal)
+			ciudad1 = new Ciudad(101, null, "5360");
+			
+		}catch(ExceptionNulo exception) {
+			
+			assertEquals("El nombre de la ciudad no puede ser nulo", ciudad1.getMessage());
+			
+		}
+		
+		
+		try {
+			
+			//(IdCIudad, nombre, codigoPostal)
+			ciudad1 = new Ciudad(101, "", "5360");
+			
+		}catch(ExceptionVacio exception) {
+			
+			assertEquals("El nombre de la ciudad no puede ser vacio", ciudad1.getMessage());
+			
+		}
+		
+		
+		try {
+			
+			//(IdCIudad, nombre, codigoPostal)
+			ciudad1 = new Ciudad(101, "Chilecito", null);
+			
+		}catch(ExceptionNulo exception) {
+			
+			assertEquals("El codigo postal no puede ser nulo", ciudad1.getMessage());
+			
+		}
+		
+		
+		try {
+			
+			//(IdCIudad, nombre, codigoPostal)
+			ciudad1 = new Ciudad(101, "Chilecito", "");
+			
+		}catch(ExceptionVacio exception) {
+			
+			assertEquals("El codigo postal no puede ser vacio", ciudad1.getMessage());
+			
+		}
+		
+	
+	}
+	
+	
+	
 }
