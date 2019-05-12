78ab78e9dec84fab6f47d9a4febecb5b4f504b57
Soria Jose <soriaajose@gmail.com>
2019-05-12 17:06:14 -0300

    "Se creo test para clase Pasajero"
    Se evalúa que los atributos no esten vacios y tampoco nulos. Ademas se agrego test para formato de CUIL.

---
+++ SoriaAeropuerto/src/test/java/test/TestCiudad.java
@@ -0,0 +1,17 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCiudad {
+
+	@Test
+	void test_Parametros_Ciudad() {
+		
+		
+		
+		
+	}
+
+}
---
+++ SoriaAeropuerto/src/test/java/test/TestPasajero.java
@@ -0,0 +1,173 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;import org.junit.rules.ExpectedException;
+
+class TestPasajero {
+
+	@Test
+	void test_ParametrosPasajero() {
+		
+		try {
+			
+			Pasajero pasajero1 = new Pasajero(null, "201234561", "Soria", "Jose", "15123456");
+			fail("Esta lina no deberia correrse");
+			
+		}catch(ExceptionNulo exception) {
+			
+			assertEquals("El ID no puede ser nulo", exception.getMenssage());
+			
+		}
+		
+		
+		try {
+			
+			Pasajero pasajero1 = new Pasajero("", "201234561", "Soria", "Jose", "15123456");
+			fail("Esta lina no deberia correrse");
+			
+		}catch(ExceptionVacio exception) {
+			
+			assertEquals("El ID no puede ser vacio", exception.getMenssage());
+			
+		}
+		
+		
+		try {
+			
+			Pasajero pasajero1 = new Pasajero(1001, null, "Soria", "Jose", "15123456");
+			fail("Esta lina no deberia correrse");
+			
+		}catch(ExceptionNulo exception) {
+			
+			assertEquals("El CUIL no puede ser nulo", exception.getMenssage());
+			
+		}
+		
+		try {
+			
+			Pasajero pasajero1 = new Pasajero(1001, "", "Soria", "Jose", "15123456");
+			fail("Esta lina no deberia correrse");
+			
+		}catch(ExceptionVacio exception) {
+			
+			assertEquals("El CUIL no puede ser vacio", exception.getMenssage());
+			
+		}
+		
+		
+		try {
+			
+			Pasajero pasajero1 = new Pasajero(1001, "201234561", null, "Jose", "15123456");	
+			fail("Esta lina no deberia correrse");
+			
+		}catch(ExceptionNulo exception) {
+			
+			assertEquals("El apellido no puede ser nulo", exception.getMenssage());
+			
+		}
+		
+		try {
+			
+			Pasajero pasajero1 = new Pasajero(1001, "201234561", "", "Jose", "15123456");	
+			fail("Esta lina no deberia correrse");
+			
+		}catch(ExceptionVacio exception) {
+			
+			assertEquals("El apellido no puede ser vacio", exception.getMenssage());
+			
+		}
+		
+		try {
+			
+			Pasajero pasajero1 = new Pasajero(1001, "201234561", "Soria", null, "15123456");	
+			fail("Esta lina no deberia correrse");
+			
+		}catch(ExceptionNulo exception) {
+			
+			assertEquals("El nombre no puede ser nulo", exception.getMenssage());
+			
+		}
+		
+		try {
+			
+			Pasajero pasajero1 = new Pasajero(1001, "201234561", "Soria", "", "15123456");	
+			fail("Esta lina no deberia correrse");
+			
+		}catch(ExceptionVacio exception) {
+			
+			assertEquals("El nombre no puede ser vacio", exception.getMenssage());
+			
+		}
+		
+		
+		try {
+			
+			Pasajero pasajero1 = new Pasajero(1001, "201234561", "Soria", "Jose", null);	
+			fail("Esta lina no deberia correrse");
+			
+		}catch(ExceptionNulo exception) {
+			
+			assertEquals("El telefono no puede ser nulo", exception.getMenssage());
+			
+		}
+		
+		
+		try {
+			
+			Pasajero pasajero1 = new Pasajero(1001, "201234561", "Soria", "Jose", "");	
+			fail("Esta lina no deberia correrse");
+			
+		}catch(ExceptionVacio exception) {
+			
+			assertEquals("El telefono no puede ser vacio", exception.getMenssage());
+			
+		}
+		
+		
+	}
+
+	@Test
+	void test_CUILPasajero() {
+		
+		Pasajero pasajero1 = null;
+		
+		try {		
+		
+			pasajero1 = new Pasajero(1001, "10123456781", "Soria", "Jose", "15123456");
+			fail("Esta lina no deberia correrse");
+			
+		}catch(ExceptionCantidadIncorrecta exception) {
+			
+			assertEquals("La cantidad es dinstinta a 10 y 11", exception.getMenssage());
+			
+		}
+		
+		try {		
+			
+			pasajero1 = new Pasajero(1001, "1012345678134", "Soria", "Jose", "15123456");
+			fail("Esta lina no deberia correrse");
+			
+		}catch(ExceptionCantidadIncorrecta exception) {
+			
+			assertEquals("La cantidad es dinstinta a 10 y 11", exception.getMenssage());
+			
+		}
+		
+		
+		//Test para el formato del documento. 
+		try {
+		
+			pasajero1 = new Pasajero(1001, "10123456781", "Soria", "Jose", "15123456");
+			assertEquals("El numero de CUIL es: 10-12345678-1", pasajero1.getCUILFormato());
+			
+		}catch(ExceptionCantidadIncorrecta exception) {
+			
+			fail("Esta linea no deberia correrse");
+			
+		}
+		
+	}
+	
+
+}
