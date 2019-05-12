beed2eefcbc6cf380e9716e179570ddaa437b2ef
Soria Jose <soriaajose@gmail.com>
2019-05-12 18:12:22 -0300

    "Se creo test para la clase Aerolinea"

---
+++ SoriaAeropuerto/src/test/java/test/TestAerolineas.java
@@ -0,0 +1,78 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestAerolineas {
+
+	@Test
+	void test_Atributos_Aerolineas() {
+		
+		Aerolinea aerolinea1 = null;
+		
+		try {
+			
+			//(idAerolinea, nombre)
+			aerolinea1 = new Aerolinea(1234, "Fly Emirates");
+			fail("Esta linea no deberia correrse");
+			
+		}catch(Exception exception) {
+			
+			fail("Esta liea no deberia correrse");
+			
+		}
+		
+		
+		try {
+			
+			aerolinea1 = new Aerolinea(null, "Fly Emirates");
+			fail("Esta linea no deberia correrse");
+			
+		}catch(ExceptionNulo exception) {
+			
+			
+			assertEquals("El ID de la Aerolinea no puede ser nulo", exception.getMessage());
+		}
+		
+		
+		try {
+			
+			aerolinea1 = new Aerolinea("", "Fly Emirates");
+			fail("Esta linea no deberia correrse");
+			
+		}catch(ExceptionVacio exception) {
+			
+			assertEquals("El ID de la Aerolinea no puede ser vacio", exception.getMessage());
+		}
+		
+
+		try {
+			
+			aerolinea1 = new Aerolinea(1234, "");
+			fail("Esta linea no deberia correrse");
+			
+		}catch(ExceptionVacio exception) {
+			
+			assertEquals("El nombre de la Aerolinea no puede ser vacio", exception.getMessage());
+		}
+		
+		
+		try {
+			
+			aerolinea1 = new Aerolinea(1234, null);
+			fail("Esta linea no deberia correrse");
+			
+		}catch(ExceptionNulo exception) {
+			
+			assertEquals("El nombre de la Aerolinea no puede ser nulo", exception.getMessage());
+		}
+		
+		
+	}
+
+	
+
+	
+	
+}
