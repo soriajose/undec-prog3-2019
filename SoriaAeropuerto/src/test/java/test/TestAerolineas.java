8c4d169b87f7f208f34fe4cd4c5cd1739bcf10c5
SoriaJose <soriaajose@gmail.com>
2019-08-27 19:19:10 -0300

    "Se modifico la clase TestAerolinea"

--- SoriaAeropuerto/src/test/java/test/TestAerolineas.java
+++ SoriaAeropuerto/src/test/java/test/TestAerolineas.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import dominio.Aerolinea;
@@ -9,104 +10,25 @@ import excepciones.ExceptionNulo;
 import excepciones.ExceptionVacio;
 
 class TestAerolineas {
-
+	
 	@Test
-	void test_Atributos_Aerolineas() throws ExceptionVacio, ExceptionNulo {
+	void factoryAerolinea_todoLosAtributos_instanciaIncorrecta() {
 		
-		Aerolinea aerolinea1 = null;
-		
-		try {
-			
-			aerolinea1 = new Aerolinea(1234, "Fly Emirates");
-			
-		}catch(Exception exception) {
-			
-			fail("Esta liea no deberia correrse");
-			
-		}
-		
-		
-		try {
-			
-			aerolinea1 = new Aerolinea(null, "Fly Emirates");
-			
-		}catch(ExceptionNulo exception) {
-			
-			assertEquals("El ID de la Aerolinea no puede ser nulo", exception.getMessage());
-		}
-		
-		
-		try {
-			
-			aerolinea1 = new Aerolinea(1234, "");
-			
-		}catch(ExceptionVacio exception) {
-			
-			assertEquals("El nombre de la Aerolinea no puede ser vacio", exception.getMessage());
-		}
-		
-		
-		try {
-			
-			aerolinea1 = new Aerolinea(1234, null);
-			
-		}catch(ExceptionNulo exception) {
-			
-			assertEquals("El nombre de la Aerolinea no puede ser nulo", exception.getMessage());
-		}
+		Assertions.assertThrows(ExceptionNulo.class, ()-> Aerolinea.factoryAerolinea(null, "Qatar Airways"));
+		Assertions.assertThrows(ExceptionNulo.class, ()-> Aerolinea.factoryAerolinea(2001, null));
+		Assertions.assertThrows(ExceptionNulo.class, ()-> Aerolinea.factoryAerolinea(2001, ""));
+		Assertions.assertThrows(ExceptionNulo.class, ()-> Aerolinea.factoryAerolinea(null, null));
 		
 		
 	}
 
 	
 	@Test
-	void test_Resumen_Aerolineas() {
+	void testAerolinea_todosLosAtributos_instanciaCorrecta() throws ExceptionNulo {
 		
-		Aerolinea aerolinea = null;
-		
-		try {
-			
-			aerolinea = new Aerolinea(1234, "Fly Emirates");
-			
-			assertEquals("ID Aerolinea: 1234 - Nombre: Fly Emirates", aerolinea.getResumenAerolinea());
-			
-			
-		}catch(Exception exception) {
-			
-			fail("Esta lina no deberia correrse");
-		}
-		
-		
-		try {
-			
-			aerolinea = new Aerolinea(1235, "Qatar Airways");
-			
-			assertEquals("ID Aerolinea: 1235 - Nombre: Qatar Airways", aerolinea.getResumenAerolinea());
-			
-			
-		}catch(Exception exception) {
-			
-			fail("Esta lina no deberia correrse");
-		}
-		
-		try {
-			
-			aerolinea = new Aerolinea(1236, "Eva Air");
-			
-			assertEquals("ID Aerolinea: 1236 - Nombre: Eva Air", aerolinea.getResumenAerolinea());
-			
-			
-		}catch(Exception exception) {
-			
-			fail("Esta lina no deberia correrse");
-		}
-		
-		
+		Aerolinea aerolinea = Aerolinea.factoryAerolinea(2001, "Qatar Airways");
 		
 	}
-
-	
-	
 	
 	
 	
